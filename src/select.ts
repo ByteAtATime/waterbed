49a1bdb2ec2529d503e4a2dca6f54331fb697e54
ByteAtATime <byteatatime@proton.me>
2025-08-02 12:46:10 -0700

    feat: make select query builder actually a builder

--- src/select.ts
+++ src/select.ts
@@ -6,13 +6,27 @@ import type {
   InferSelectModel,
 } from "./schema";
 
-export class SelectQueryBuilder {
-  constructor(private base: Airtable.Base) {}
+export class SelectQuery<T extends AirtableTable>
+  implements PromiseLike<InferSelectModel<T>[]>
+{
+  private _filterByFormula: string | null = null;
 
-  public async from<T extends AirtableTable>(table: T) {
-    const tableName = table._tableName;
+  constructor(private base: Airtable.Base, private table: T) {}
 
-    const records = await this.base(tableName).select().all();
+  public filter(filterFormula: string): this {
+    this._filterByFormula = filterFormula;
+    return this;
+  }
+
+  private async execute(): Promise<InferSelectModel<T>[]> {
+    const tableName = this.table._tableName;
+
+    const query = this.base(tableName).select({
+      filterByFormula: this._filterByFormula ?? undefined,
+    });
+
+    const records = await query.all();
+    const table = this.table;
 
     return records.map((record) => {
       const { _tableName, ...fields } = table;
@@ -28,5 +42,26 @@ export class SelectQueryBuilder {
 
       return result as InferSelectModel<T>;
     });
+  }
+
+  public then<TResult1 = InferSelectModel<T>[], TResult2 = never>(
+    onfulfilled?:
+      | ((value: InferSelectModel<T>[]) => TResult1 | PromiseLike<TResult1>)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+      | undefined
+      | null
+  ): PromiseLike<TResult1 | TResult2> {
+    return this.execute().then(onfulfilled, onrejected);
+  }
+}
+
+export class SelectQueryBuilder {
+  constructor(private base: Airtable.Base) {}
+
+  public from<T extends AirtableTable>(table: T): SelectQuery<T> {
+    return new SelectQuery(this.base, table);
   }
 }
